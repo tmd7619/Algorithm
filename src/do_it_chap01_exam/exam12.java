86725b70e67a709969900dda2f9bf230b34d3ca1
tmd7619 <tmd814@naver.com>
2021-08-07 15:24:41 +0900

    study chap01, chap02

---
+++ src/do_it_chap01_exam/exam12.java
@@ -0,0 +1,27 @@
+package do_it_chap01_exam;
+
+public class exam12 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("--|1\t2\t3\t4\t5\t6\t7\t8\t9");
+		System.out.println("--*---------------------------------------------------------------");
+		
+		for(int i = 1 ; i <= 9 ; i ++) {
+			
+			System.out.print(i + " |");
+			for(int j = 1 ; j <= 9 ;  j++) {
+				System.out.print(j*i+"\t");
+				if(j == 9) {
+					System.out.println();
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
