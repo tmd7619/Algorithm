6862caca0fbe6998e046d293702767389eb4c8fe
tmd7619 <tmd814@naver.com>
2021-08-16 10:57:25 +0900

    study improve BubbleSort

---
+++ src/chap06/sorting/exam/BubbleSort02.java
@@ -0,0 +1,57 @@
+package chap06.sorting.exam;
+
+import java.util.Scanner;
+
+// 버블 정렬(버전 2) -> '멈춤'으로 개선한 버블 정렬 메서드
+// 어떤 패스에서 요소의 교환 횟수가 0이면 더 이상 정렬할 요소가 없음 -> 정렬작업 멈춤
+
+
+public class BubbleSort02 {
+	
+	// a[idx1]와 [a[idx2]의 값을 바꾼다
+	static void swap(int[] a, int idx1, int idx2) {
+		int temp = a[idx1] ;  
+		a[idx1] = a[idx2]; // a[idx2] to a[idx1]
+		a[idx2] = temp ; // temp to a[idx2]
+	}
+	
+	// 버블정렬 (버전2)
+	static void bubbleSort(int[] a , int n ) { // n : 배열의 size
+		for(int i = 0; i < n-1 ; i ++) {
+			int exchg = 0 ; 			// 패스의 교환횟수를 기록 // 다시 패스를 돌때 0으로 초기화
+			for(int j = n -1 ; j > i ; j --) {
+				if(a[j-1] > a[j]) {
+					swap(a,j-1,j);
+					exchg++;	// 패스에서 요소를 교환할 때 마다 1씩 증가
+				}
+			}
+			if(exchg==0) break;		// 교환이 이루어지지 않으면 종료
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("버블 정렬(버젼2)");
+		System.out.print("요솟수 : ");
+		int nx = sc.nextInt();
+		sc.nextLine();
+		
+		int[] x  = new int[nx]; // nx size로 int[] x 배열 초기화
+		
+		for(int i = 0 ; i < nx; i ++) { // 요솟값 입력
+			System.out.print("x[" + i + "] : ");
+			x[i] = sc.nextInt();
+			sc.nextLine();
+		}
+		
+		bubbleSort(x, nx); // 배열 x를 버블정렬
+		
+		System.out.println("오름차순으로 정렬했습니다");
+		for(int i = 0 ; i < nx; i ++) {
+			System.out.println("x[" + i + "] = "+ x[i]);
+		}
+		
+	}
+	
+}
---
+++ src/chap06/sorting/exam/BubbleSort03.java
@@ -0,0 +1,62 @@
+package chap06.sorting.exam;
+
+import java.util.Scanner;
+
+// 버블 정렬(버전 3) -> 앞쪽 정렬이 이미 되있다면, 그 인덱스까지 패스 탐색을 하지 않고 정렬이 필요한 요소만 패스 탐색 실행
+
+
+public class BubbleSort03 {
+	
+	static int cnt = 0 ;// 패스 교환 횟수
+	
+	// a[idx1]와 [a[idx2]의 값을 바꾼다
+	static void swap(int[] a, int idx1, int idx2) {
+		int temp = a[idx1] ;  
+		a[idx1] = a[idx2]; // a[idx2] to a[idx1]
+		a[idx2] = temp ; // temp to a[idx2]
+		
+	}
+	
+	// 버블정렬 (버전3)
+	static void bubbleSort(int[] a , int n ) { // n : 배열의 size
+		int k = 0 ; 	// a[k] 보다 앞쪽은 이미 정렬을 마친 상태
+		while(k < n -1) {
+			int last = n-1; // 마지막으로 요소를 교환한 위치 // 각 패스에서 마지막으로 교환한 두 요소 가운데 오른쪽 요소a[j]를 저장하기 위한 변수
+			for(int j = n-1 ; j > k ; j--) {
+				if(a[j-1] > a[j] ) {
+					swap(a,j-1,j);
+					cnt++;
+					last = j; // 여기서의 j는 swap 된 이후의 값
+				}
+			}
+			k = last; // last 값을 k에 대입하여 수행할 패스의 범위 제한
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("버블 정렬(버젼3)");
+		System.out.print("요솟수 : ");
+		int nx = sc.nextInt();
+		sc.nextLine();
+		
+		int[] x  = new int[nx]; // nx size로 int[] x 배열 초기화
+		
+		for(int i = 0 ; i < nx; i ++) { // 요솟값 입력
+			System.out.print("x[" + i + "] : ");
+			x[i] = sc.nextInt();
+			sc.nextLine();
+		}
+		
+		bubbleSort(x, nx); // 배열 x를 버블정렬
+		
+		System.out.println("오름차순으로 정렬했습니다");
+		for(int i = 0 ; i < nx; i ++) {
+			System.out.println("x[" + i + "] = "+ x[i]);
+		}
+		System.out.println("패스 교환 횟수 : "+ cnt);
+		
+	}
+	
+}
